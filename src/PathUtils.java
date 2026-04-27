7599454fc209d2ea8d6bbca3200fdbe0699dfed5
gaelsoto <gsoto1@unm.edu>
2026-04-27 13:47:02 -0600

    formatting method for the List

--- src/PathUtils.java
+++ src/PathUtils.java
@@ -23,4 +23,14 @@ public class PathUtils {
         }
         return path;
     }
+
+    public static String format(List<Integer> path, Graph graph) {
+        if (path.isEmpty()) return "(no path found)";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < path.size(); i++) {
+            if (i > 0) sb.append(" -> ");
+            sb.append(graph.getLabel(path.get(i)));
+        }
+        return sb.toString();
+    }
 }
