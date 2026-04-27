0bf3c0abfd12c9a0c09834f95be76cfae7ff7419
gaelsoto <gsoto1@unm.edu>
2026-04-27 14:24:59 -0600

    hard-coded graph B

--- src/ExampleGraph.java
+++ src/ExampleGraph.java
@@ -11,4 +11,31 @@ public class ExampleGraph {
         for (int[] e : EDGES_A) g.addEdge(e[0], e[1]);
         return g;
     }
+
+    //build graph b
+    private static final String[] LABELS_B =
+            {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"};
+
+    private static final int[][] EDGES_B = {
+            {0,1},{0,2},           // A->B, A->C
+            {1,3},                 // B->D
+            {2,3},                 // C->D
+            {3,4},                 // D->E
+            {4,5},{4,6},           // E->F, E->G
+            {5,7},{5,3},           // F->H, F->D
+            {6,13},{6,10},{6,7},   // G->N, G->K, G->H
+            {7,8},                 // H->I
+            {8,14},{8,9},          // I->O, I->J
+            {9,3},                 // J->D
+            {10,11},               // K->L
+            {11,14},{11,6},        // L->O, L->G
+            {12,14},{12,6},        // M->O, M->G
+            {13,14},{13,11}        // N->O, N->L
+    };
+
+    public static Graph buildGraphB() {
+        Graph g = new Graph(15, true, LABELS_B);  // 15 vertices, directed
+        for (int[] e : EDGES_B) g.addEdge(e[0], e[1]);
+        return g;
+    }
 }
