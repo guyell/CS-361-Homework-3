3220a85ad59ed18bc40b6604032c9aa64f9c5db8
gaelsoto <gsoto1@unm.edu>
2026-04-27 13:40:51 -0600

    reconstruct method depending on which graph is used. Returns path

--- src/PathUtils.java
+++ src/PathUtils.java
@@ -1,2 +1,26 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class PathUtils {
+
+    public static List<Integer> reconstruct(Map<Integer, Integer> parent, int end) {
+        LinkedList<Integer> path = new LinkedList<>();
+        int current = end;
+        while (current != -1) {
+            path.addFirst(current);
+            current = parent.get(current);
+        }
+        return path;
+    }
+
+    public static List<Integer> reconstruct(int[] parent, int end) {
+        LinkedList<Integer> path = new LinkedList<>();
+        int current = end;
+        while (current != -1) {
+            path.addFirst(current);
+            current = parent[current];
+        }
+        return path;
+    }
 }
