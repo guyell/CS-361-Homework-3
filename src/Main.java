b69277e8b840f61b42544e150c09f774d84f95b0
gaelsoto <gsoto1@unm.edu>
2026-04-27 18:12:29 -0600

    finished implementation of clases for Graph B

--- src/Main.java
+++ src/Main.java
@@ -25,5 +25,26 @@ public class Main{
                 bfsA_matrix.path, dfsA_matrix.path);
 
         Printer.printBenchmarks(bfsA_list, dfsA_list, bfsA_matrix, dfsA_matrix);
+
+        //for graph b
+        Printer.separator("GRAPH (b)  |  Find path: A → O");
+
+        Graph graphB = ExampleGraph.buildGraphB();
+        Printer.printGraph(graphB);
+
+        // A = index 0,  O = index 14
+        int A = graphB.getVertex("A");
+        int O = graphB.getVertex("O");
+
+        Benchmark.Result bfsB_list   = Benchmark.run(() -> bfs.searchList  (graphB, A, O));
+        Benchmark.Result dfsB_list   = Benchmark.run(() -> dfs.searchList  (graphB, A, O));
+        Benchmark.Result bfsB_matrix = Benchmark.run(() -> bfs.searchMatrix(graphB, A, O));
+        Benchmark.Result dfsB_matrix = Benchmark.run(() -> dfs.searchMatrix(graphB, A, O));
+
+        Printer.printPaths(graphB,
+                bfsB_list.path, dfsB_list.path,
+                bfsB_matrix.path, dfsB_matrix.path);
+
+        Printer.printBenchmarks(bfsB_list, dfsB_list, bfsB_matrix, dfsB_matrix);
     }
 }
