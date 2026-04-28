70af33a4dd2272909a79aab0b7f48b849dc1861d
gaelsoto <gsoto1@unm.edu>
2026-04-27 18:01:14 -0600

    printing method for each graph

--- src/Print.java
+++ src/Print.java
@@ -1,2 +1,22 @@
 public class Print {
+    //header
+    public static void separator(String title) {
+        System.out.println("\n" + "═".repeat(65));
+        System.out.println("  " + title);
+        System.out.println("═".repeat(65));
+    }
+
+    public static void section(String title) {
+        int pad = Math.max(0, 60 - title.length());
+        System.out.println("\n " + title + " " + "─".repeat(pad));
+    }
+
+    //graphs
+    public static void printGraph(Graph graph) {
+        section("Adjacency List");
+        graph.printAdjList();
+        section("Adjacency Matrix");
+        graph.printAdjMatrix();
+    }
+
 }
