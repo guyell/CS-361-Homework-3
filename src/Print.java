68f98a5a4789edeccad40824bb4102779578edd1
gaelsoto <gsoto1@unm.edu>
2026-04-27 18:02:00 -0600

    printing method for each path

--- src/Print.java
+++ src/Print.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Print {
     //header
     public static void separator(String title) {
@@ -17,6 +19,22 @@ public class Print {
         graph.printAdjList();
         section("Adjacency Matrix");
         graph.printAdjMatrix();
+    }
+
+    public static void printPaths(Graph graph,
+                                  List<Integer> bfsList,
+                                  List<Integer> dfsList,
+                                  List<Integer> bfsMatrix,
+                                  List<Integer> dfsMatrix) {
+        section("Paths found");
+        System.out.printf(" %-22s : %s  [%d nodes]%n",
+                "BFS (adj-list)",   PathUtils.format(bfsList,   graph), bfsList.size());
+        System.out.printf("  %-22s : %s  [%d nodes]%n",
+                "DFS (adj-list)",   PathUtils.format(dfsList,   graph), dfsList.size());
+        System.out.printf("  %-22s : %s  [%d nodes]%n",
+                "BFS (adj-matrix)", PathUtils.format(bfsMatrix, graph), bfsMatrix.size());
+        System.out.printf("  %-22s : %s  [%d nodes]%n",
+                "DFS (adj-matrix)", PathUtils.format(dfsMatrix, graph), dfsMatrix.size());
     }
 
 }
